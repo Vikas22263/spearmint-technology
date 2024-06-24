dec17b58599fe237c427d0aa16642c4b63e62ef9
vikas patel <109585264+Vikas22263@users.noreply.github.com>
2024-06-24 14:38:58 +0530

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,28 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
   const [imagePosition, setImagePosition] = useState({ x: 0, y: 0 });
 
-  const handleclick = (e) => {
-    const containerRect = e.currentTarget.getBoundingClientRect();
-    const offsetX = e.clientX - containerRect.left;
-    const offsetY = e.clientY - containerRect.top;
-    
- 
+  const handleClick = (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
     const newPosition = {
-      x: offsetX - 50, 
-      y: offsetY - 50, 
+      x: event.clientX - 50,
+      y: event.clientY - 50,
     };
-
     setImagePosition(newPosition);
   };
 
   return (
-    <div onClick={handleclick} style={{ position: 'relative', height: '100vh' }}>
+    <div onClick={handleClick} style={{ position: 'relative', height: '100vh' }}>
       <img
         style={{
           width: '100px',
           height: '100px',
           position: 'absolute',
-          left: `${imagePosition.x}px`, 
+          left: `${imagePosition.x}px`,
           top: `${imagePosition.y}px`,
-          transition: ' 1s ease',
         }}
-        src='/image/wizaart-img.56787174.gif'
+        src={process.env.PUBLIC_URL + '/image/wizaart-img.56787174.gif'}
         alt=""
       />
     </div>
